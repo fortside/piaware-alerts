22a7b573f64164f61801d798bbb921cc83c4a8c7
Neal Harder <nealharder@gmail.com>
2018-06-16 13:43:21 -0600

    updated nightly backup script, didn't realize the new constant added broke it. simple fix.

--- nightlybackup.sh
+++ nightlybackup.sh
@@ -1,5 +1,5 @@
 #parse through the constants file to get the name of the database
-dbname=$(cat ~/piaware-alerts/constants.py | grep "db_name" | cut -d'"' -f 2)
+dbname=$(cat ~/piaware-alerts/constants.py | grep "db_name" -m 1| cut -d'"' -f 2)
 #get the date
 today=$(date +%Y-%m-%d)
 #create a copy of today's database with the date prepended to it
